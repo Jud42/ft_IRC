8821ebe95a2f287b066ddb2c2417f74cdabe91ac
exonsecfl <exo@nsecfl.ch>
2023-04-04 18:49:15 +0200

    Corr err empty parameters

--- srcs/Server/Server_stop.cpp
+++ srcs/Server/Server_stop.cpp
@@ -3,18 +3,22 @@
 void	Server::stop(void)
 {  
 	
-  //do
-	//{
-    //int rc;
-		//rc = recv(fds[i].fd, buf sizeof(buf), 0);
-    //    if (rc < 0)
-    //    {
-    //        if (errno != EWOULDBLOCK)
-    //        {
-    //          perror("  recv() failed");
-    //          //close_conn = TRUE;
-    //        }
-  //          break;
-	//}
-  
+    int res;
+    struct sockaddr_storage peer_addr;
+    socklen_t peer_addr_len;
+
+	
+		res = recvfrom(_listener, _buffer, BUFFER_SIZE, 0,
+					(struct sockaddr *) &peer_addr, &peer_addr_len); 
+		//res = recv(fds[i].fd, buf sizeof(buf), 0);
+		if (res < 0)
+		{
+			if (errno != EWOULDBLOCK)
+			{
+				perror("  recv() failed");
+				//close_conn = TRUE;
+			}
+		}
+
+
 }
