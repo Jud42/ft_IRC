0c4181fdbecabf70e0fe16670cafe87f9097a58b
Vanessa Haefeli <vhaefeli@student.42lausanne.ch>
2023-04-06 10:06:34 +0200

    small texte correction

--- srcs/Server/Server_listening.cpp
+++ srcs/Server/Server_listening.cpp
@@ -3,16 +3,16 @@
 void Server::listening(void)
 {
 	// review the list of address structures returned by getaddrinfo.
-    // stop when the the socket and bind calls are successful. 
+    // stop when the socket and bind calls are successful.
 
-    
+
     int  optval = 1; // required by setsockopt : std value
 	struct addrinfo *rptr;
 	// pass over all addresse, next is proving the next refernece
-	for (rptr = _addrs; rptr != NULL; rptr = rptr->ai_next) 
+	for (rptr = _addrs; rptr != NULL; rptr = rptr->ai_next)
     {
         // creates an endpoint for communication and returns a descriptor.
-		// The socket has the indicated type, which specifies the communication semantics. 
+		// The socket has the indicated type, which specifies the communication semantics.
 		_listener = socket (rptr->ai_family, rptr->ai_socktype, rptr->ai_protocol);
 
 		if ("DEBUG" ==_IRCconfig->getConfigValue("DEBUG")) // -------------------------------------
@@ -30,16 +30,16 @@ void Server::listening(void)
 
 		// configure socket
 		// SOL_SOCKET for all sockets
-		// SO_REUSEADDR indicates that the rules used in validating addresses supplied in 
-		// a bind(2) call should allow reuse of local addresses. 
+		// SO_REUSEADDR indicates that the rules used in validating addresses supplied in
+		// a bind(2) call should allow reuse of local addresses.
 		int res = setsockopt (_listener, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof (int));
 
 		if (res == -1)
 		{
 			throw std::runtime_error("[SERVER_LISTENING] - ERROR Setsockopt failed") ;
 		}
-		
-		// bind the socket to an host::port 
+
+		// bind the socket to an host::port
 		res = bind (_listener, rptr->ai_addr, rptr->ai_addrlen);
 
 		if ("DEBUG" == _IRCconfig->getConfigValue("DEBUG")) // -------------------------------------
@@ -47,7 +47,7 @@ void Server::listening(void)
 			std::cout << BLU;
 			std::cout << "[SERVER_LISTENING] - DEBUG Bind result <" << res << ">" << std::endl;
 			std::cout << NOC;
-		} // ---------------------------------------------------------------------------------------		
+		} // ---------------------------------------------------------------------------------------
 
 		if (!res)  // Success
 		{
@@ -58,7 +58,7 @@ void Server::listening(void)
 	if (!rptr)
 		throw std::runtime_error("[SERVER_LISTENING] - ERROR Binding failed");
 
-	// Put the socker in a listening mod. Ready to reveive. 
+	// Put the socker in a listening mod. Ready to reveive.
 	//	BACKLOG = socket's queue size after what ECONNREFUSED ist return
 	if (listen (_listener, BACKLOG) == -1)
 		throw std::runtime_error("[SERVER_LISTENING] - ERROR Listen failed");
