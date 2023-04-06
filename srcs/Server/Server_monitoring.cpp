524ae62ba9c8c36ad4e8411ab7568774f777e19b
Jud <92598232+Jud42@users.noreply.github.com>
2023-04-06 09:44:10 +0200

    Revert "Local rmamison"

---
+++ srcs/Server/Server_monitoring.cpp
@@ -0,0 +1,46 @@
+#include "Server.hpp"
+
+void Server::monitoring( void )
+{
+	// poll_create a new poll instance.
+	// _efd = epoll_create1 (0);
+
+
+	//if (_efd < 0)
+	//	throw std::runtime_error("[SERVER_MONITORING]- ERROR epoll_create1 failed");
+
+	// add listener to efd. Watch the event Epollin 
+	// int nfds  = 1;
+	
+	struct pollfd ev[200];
+	memset (ev, 0, sizeof(ev));
+	ev[0].events = POLLIN;
+	ev[0].fd = _listener;
+	int nfds = 1;
+	
+	// upadate the epoll instance with the new epoll 
+	// EPOLL_CTL_ADD = Add an entry to the interest list of the epoll file
+	//int res = epoll_ctl (_efd, EPOLL_CTL_ADD, _listener, &ev);
+
+
+	int res = poll(ev, nfds, TIMEOUT);
+	if ("DEBUG" ==_IRCconfig->getConfigValue("DEBUG")) // -------------------------------------
+	{
+		std::cout << BLU;		
+		std::cout << "[SERVER_MONITORING] DEBUG - Provide poll value" << std::endl;
+		std::cout << " res <" << res << ">" << std::endl;
+		std::cout << " ev.events <" << ev->events << ">" << std::endl;
+		std::cout << " ev.fd <" << ev->fd << ">" << std::endl;
+		std::cout << " ev.revents <" << ev->revents << ">" << std::endl;
+		std::cout << " nfds <" << nfds << ">" << std::endl;
+		std::cout << NOC;
+	} // ---------------------------------------------------------------------------------------	
+	if (res < 0)
+		throw std::runtime_error("[SERVER_MONITORING]- ERROR poll failed");
+	//if (res == 0)
+	//	throw std::runtime_error("[SERVER_MONITORING]- ERROR poll timed out");		
+
+	
+
+
+}
---
+++ srcs/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "Server.hpp"
+#include "ConfigFile.hpp"
+
+int main (int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cerr << "[MAIN] Require execution paramaters like :" << argv[0] << " <PORT> <PASSWORD>" << std::endl;
+		return(0);
+    }
+	// as per document RFC7194, ports are following hit the concensus
+	// 6667 for incommint IRC connections non encrypted
+	// 6697 for incomming IRC connections encrypted
+
+	// launch the irc.config loading for on-demand use
+	
+	ConfigFile IRCconfig("./srcs/irc.config");
+
+	// std::string valueConfig = IRCconfig.getConfigValue("DEBUG");
+	// return flase on other input 
+	// bool DEBUG = (valueConfig == "DEBUG");
+
+	// define the server
+	Server *srv = NULL;
+	try 
+	{ 
+		srv = new Server(argv[1], argv[2], &IRCconfig);
+	}
+	catch (std::exception &	e)
+	{
+		std::cerr <<"[MAIN] - Exception catched during srv INIT: " << e.what() << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	
+	srv->test();
+
+	
+	srv->stop();
+	delete srv;
+	exit(EXIT_SUCCESS);
+	
+}
