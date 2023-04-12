347658f40bef5912b5efd692434169e128815c85
Vhaefeli <vhaefeli@student.42lausanne.ch>
2023-04-12 11:31:18 +0200

    test in the train

--- srcs/Server/Server_monitoring.cpp
+++ srcs/Server/Server_monitoring.cpp
@@ -19,19 +19,19 @@ void Server::monitoring( void )
 
 	std::cout << test.fd << std::endl;
 
-	while (true) {
-
+	while (true) 
+	{
 		//wait evenement
 		std::cout << "start" << std::endl;
 		if (poll(*fds.data(), fds.size(), TIMEOUT) <= 0)
 				throw std::runtime_error("[SERVER_MONITORING] - ERROR poll() failed: timeout");
 		std::cout << "poll activation" << std::endl;
 		//iterate each fd for check event
-		std::cout << "POLLIN" << POLLIN << std::endl;
-		std::cout << "fds.front()->revents" << fds.front()->revents << std::endl;
+		std::cout << "POLLIN " << POLLIN << std::endl;
+		std::cout << "fds.front()->fd " << fds.front()->fd << std::endl;
 
-		if (fds.front()->revents & POLLIN) {
-
+		if (fds.front()->events & POLLIN) 
+		{
 			std::cout << "je passe" << std::endl;
 			if (_nb_client == MAX_CLIENTS)
 				;//manage max client
@@ -40,42 +40,35 @@ void Server::monitoring( void )
 			std::cout << "new client_fd:" << client_fd << std::endl;
 			if (client_fd == -1)
 				throw std::runtime_error("[SERVER_MONITORING] - ERROR binding() failed");
-			std::cout << "nb client" << _nb_client << std::endl;
 			//add new fd_client in vector pollfd
-			if (client_fd > (_nb_client + 3))
+			treatment(_listener);
+			pollfd *pollclient = new pollfd;
+			pollclient->fd = client_fd;
+			pollclient->events = POLLIN;
+			pollclient->revents = 0;
+			fds.push_back(pollclient);
+			_nb_client++;
+			fds.front()->revents = 0;
+			std::string a("welcome");
+			send(client_fd, &a, a.size(), 0);
+		}
+		// else
+		// {
+			std::vector< pollfd * >::iterator it = fds.begin();
+			std::vector< pollfd * >::iterator it_end = fds.end();
+			for (it += 1; it != it_end; it++)
 			{
-				std::cout << "nb client" << _nb_client << std::endl;
-				treatment(client_fd);
-				pollfd *pollclient = new pollfd;
-				pollclient->fd = client_fd;
-				pollclient->events = POLLIN;
-				pollclient->revents = 0;
-				fds.push_back(pollclient);
-				_nb_client++;
-				std::string a("welcome");
-				// send(client_fd, &a, a.size(), 0);
-			// }
-			else
-			{
-				std::vector< pollfd * >::iterator it = fds.begin();
-				std::vector< pollfd * >::iterator it_end = fds.end();
-				for (it += 1; it != it_end; it++) {
 				std::cout << "*fd:" << (*it)->fd << std::endl;
-					if ((*it)->revents == POLLIN)
-					{
-						std::cout << "_fd:" << (*it)->fd << std::endl;
-						treatment((*it)->fd);
-						std::cout << "receive" << std::endl;
-					}
+				if ((*it)->revents == POLLIN)
+				{
+					std::cout << "_fd:" << (*it)->fd << std::endl;
+					treatment((*it)->fd);
+					std::cout << "receive" << std::endl;
 				}
 			}
+		// }
 			// continue ;
-
-		}
-
-
 	}
-
 	//close(_listener);
 	//close() //all fd_client
 }
