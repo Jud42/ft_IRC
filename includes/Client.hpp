e03879103854e285a110aa00004a19a63ca4a8f0
Vanessa Haefeli <vhaefeli@student.42lausanne.ch>
2023-04-17 10:01:35 +0200

    update channel in client.hpp

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -7,27 +7,32 @@
 # include "ConfigFile.hpp"
 # include "Server.hpp"
 
+class Channel;
+
 class Client
 {
 	private:
 
-		std::string			_nickname;
-		std::string			_username;
+		std::string				_nickname;
+		std::string				_username;
 		// std::string			_realname;
-		std::string			_password;
-		std::string			_modes;
-		std::vector<std::string>	_canal;
-		int					_clientFd;
+		std::string				_password;
+		std::string				_modes;
+		std::vector<Channel>	_channel;
+		int						_clientFd;
 		//struct sockaddr		_addrClient;
 
 	public:
 
-		void setNickname(std::string newNick);
-		std::string getNickname();
-		std::string getPassword();
-		std::string getModes();
-		std::vector<std::string>	getCanal();
-		int	getClientFd();
+		void 					setNickname(std::string newNick);
+		std::string				getNickname();
+		std::string				getPassword();
+		std::string				getModes();
+		bool					findChannel();
+		std::vector<Channel>	getChannel();
+		void					addChannel(std::vector<Channel>	channel);
+
+		int						getClientFd();
 
 		Client();
 		Client(int client_fd, char *client_data);
