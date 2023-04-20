cc9b108a6c16e8e93487840eb778425049044a68
exonsecfl <exo@nsecfl.ch>
2023-04-20 16:32:33 +0200

    part, join and channel

--- srcs/Cmds/Cmds_part.cpp
+++ srcs/Cmds/Cmds_part.cpp
@@ -2,37 +2,62 @@
 
 void Server::Cmds_part(int const fd_client, std::string const command, std::string const nickname)
 {
-    std::string pchannel = command.substr(5);
-	std::string hostname = this->_hostname;
-	std::string segment[10];
-	std::string typeC[10];
-
-	if ("DEBUG" ==_IRCconfig->getConfigValue("DEBUG")) // -------------------------------------
-	{
-		// retrieve error code of getaddrinfo command 
-		std::cout << BLU;
-        std::cout << "[ Server::Cmds_part ]" << fd_client << std::endl;
-        std::cout << "fd_client :" << fd_client << std::endl;
-        std::cout << "channel :" << pchannel << std::endl;
-    	std::cout << "nickname :" << nickname << std::endl; // WARNING missing info
-		std::cout << NOC;
-	} // --------------------------------------------------------------------------------------
-
-	// identify if manny chanels are transfered in one PART and sepparated by a comma
-	if (pchannel.find(",")==0)
-	{
-		// extract all channels - max 10;
-		
-	}
+	std::string pchannel = "";
+    // temporary code
+	if (command.find("PART") == 0)
+		pchannel = command.substr(5);
 	else
-	{
-		typeC[0] = pchannel.substr(0, 1);
-		segment[0] = pchannel.substr(1);
-	}
-	std::string cap_response = "PART " + pchannel + "\r\n";
-	cap_response = ":" + nickname + "!" + nickname + '@' + hostname + " PART " + typeC[0] + segment[0] + "\r\n";
-    //std::cout << fd_client << " [Server->Client]" << cap_response << std::endl;
+		pchannel = command;
+	//end of temporary code
+
+	std::string hostname = this->_hostname;
+
+	int max_segment = MAX_JOINS_PER_LINE;
+	std::string segment[max_segment];
+	std::string typeC[max_segment];
 	
-	send(fd_client, cap_response.c_str(), cap_response.length(), 0);
+
+	// initialise the MAX_JOINS_PER_LINE potentitial new join
+	for (int i = 0 ; i < max_segment ; i++)
+	{
+		segment[i] = "";
+		typeC[i] = "";
+	}
+
+	// identify if manny chanels are transfered in one JOIN and separated by a comma, no spaces allowed
+	if (pchannel.find(",") < pchannel.size())
+	{
+		// read each iteration of channels in one command
+		std::cout << YEL << " find " << NOC << std::endl;
+		for (int i = 0 ; i < max_segment ; i++)
+		{
+			std::cout << YEL << i << " find p =" << pchannel.find(",", 0) << NOC << std::endl;
+			if (pchannel.find(",", 0) < pchannel.size())
+			{
+				typeC[i] = pchannel.substr(0, 1);
+				segment[i] = pchannel.substr(1, pchannel.find(",")-1);
+				// reduce the size of the pchannel for the next cycle
+				pchannel = pchannel.substr(pchannel.find(",")+1);
+			}
+			else
+			{
+				typeC[i] = pchannel.substr(0, 1);
+				segment[i] = pchannel.substr(1, pchannel.find("\r"));
+				pchannel = "";
+				break;
+			}
+		}
+	}
+		
+	for (int i = 0 ; i < max_segment ; i++ )
+	{
+		if (segment[i] == "")
+			break;
+
+		std::string cap_response = "PART " + pchannel + "\r\n";
+		cap_response = ":" + nickname + "!" + nickname + '@' + hostname + " PART " + typeC[i] + segment[i] + "\r\n";
+	
+		send(fd_client, cap_response.c_str(), cap_response.length(), 0);
+	}
 
 }
