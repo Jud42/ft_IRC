fbae5cacb6c15a55212430b4bfb2994ff53eb9e7
exonsecfl <exo@nsecfl.ch>
2023-05-04 18:47:42 +0200

    add into channel the invite

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -3,6 +3,7 @@
 
 # include "define.hpp"
 # include <iostream>
+# include <vector>
 # include "ConfigFile.hpp"
 
 class Channel
@@ -12,8 +13,8 @@ public:
         Channel (std::string name, ConfigFile *IRCconfig);
         ~Channel ();
 
-        const std::string getChannelName (void);
-        const std::string getChannelMode (void);
+        const std::string                       getChannelName (void);
+        const std::string                       getChannelMode (void);
 
 
         const std::map <int, std::string>       getChannelFDsModeMap (void);
@@ -21,15 +22,17 @@ public:
         const std::string                       getChannelConnectedFDMode (const int fd);
 
 
-        int getNbUsers (void);
-        const std::string getTopic (void);
+        int                                     getNbUsers (void);
+        const std::string                       getTopic (void);
+        bool                                    checkChannelInvite(const int fd);
         
-        void    setChannelFDMode (const int fd, const std::string channelMode);
-        int     setChannelUserMode (const int fd, const std::string channelMode);
-        void    setChannelConnectedFD (const int newFD);
-        void    setChannelTopic (const std::string message);
+        void                                    setChannelFDMode (const int fd, const std::string channelMode);
+        int                                     setChannelUserMode (const int fd, const std::string channelMode);
+        void                                    setChannelConnectedFD (const int newFD);
+        void                                    setChannelTopic (const std::string message);
+        void                                    setChannelInvite (const int fd);
 
-        void    resetChannelConnectedFD (const int removeFD);
+        void                                    resetChannelConnectedFD (const int removeFD);
 
 private:
 
@@ -38,6 +41,8 @@ private:
         std::string                            _topic; // channel's topic
 
         std::map <int, std::string>            _channel_FD_Mode; // FD & mode related to the channel (mode[o, O, b, i] + owner=#)
+        std::vector <int>                      _invite; // for FD that were invited
+
 
         // for debug
         ConfigFile		               *_IRCconfig;
