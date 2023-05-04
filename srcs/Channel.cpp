f7c042c8ab589a481ed33c508444b7d0d3f37762
exonsecfl <exo@nsecfl.ch>
2023-05-04 19:08:56 +0200

    Channel's pass

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -214,12 +214,29 @@ bool Channel::checkChannelInvite(const int fd)
 }
 
 // ***********************************************************************************************
+// Return the channel's password
+const std::string Channel::getChannelPass (void)
+{
+    std::string result = this->_pass;
+
+    if ("DEBUG" == this->_IRCconfig->getConfigValue("DEBUG")) // -----------------------------
+	{
+	 	std::cout << BLU;
+        std::cout << "[ CHANNEL::channel ] getChannelPass" <<  std::endl;
+        std::cout << " result :" << result << std::endl;
+	 	std::cout << NOC;
+	} // --------------------------------------------------------------------------------------      
+    return (result);
+}
+
+// ***********************************************************************************************
 // Set the mode attached to the FD without returned value (see also setChannelUserMode)
 void Channel::setChannelFDMode (const int fd, const std::string channelMode)
 {
     std::map<int, std::string>::const_iterator it(this->_channel_FD_Mode.find(fd));
     if (it != this->_channel_FD_Mode.end())
         this->_channel_FD_Mode.at(fd) = channelMode;
+
     if ("DEBUG" == this->_IRCconfig->getConfigValue("DEBUG")) // -----------------------------
 	{
 	 	std::cout << BLU;
@@ -320,6 +337,20 @@ void Channel::setChannelInvite (const int fd)
 	} // --------------------------------------------------------------------------------------             
 }
 
+// ***********************************************************************************************
+// set the channel's password
+void Channel::setChannelPass (const std::string pass)
+{
+    this->_pass = pass;
+}
+        
+    if ("DEBUG" == this->_IRCconfig->getConfigValue("DEBUG")) // -----------------------------
+	{
+	 	std::cout << BLU;
+        std::cout << "[ CHANNEL::channel ] setChannelPass" <<  std::endl;
+        std::cout << " Pass :" << this->_pass << std::endl;
+	 	std::cout << NOC;
+	} // --------------------------------------------------------------------------------------    
 
 // ***********************************************************************************************
 // Erase the user (through the FD identification) to the given channel
